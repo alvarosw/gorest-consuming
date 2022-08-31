9e9db9270233f070a2bc8ec62aaf2a7156c9b46c
Álvaro <alvarossmiguel@gmail.com>
2022-08-31 19:22:22 -0300

    feat: delete comment

--- src/routes/comments.routes.ts
+++ src/routes/comments.routes.ts
@@ -4,5 +4,6 @@ import CommentsController from '../controllers/comments.controllers'
 const router = Router()
 
 router.get('', CommentsController.get)
+router.delete('/:id', CommentsController.remove)
 
 export default router
