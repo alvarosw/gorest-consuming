663697ca456caf2f4980df4add79ef5cec3ad9b1
Álvaro <alvarossmiguel@gmail.com>
2022-08-31 16:11:42 -0300

    feat: users crud functions

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -1,9 +1,33 @@
 import { Request, Response } from 'express'
 import GoRestApi from '../shared/gorest/GoRestApi'
 
+const api = new GoRestApi('users')
+
 export default class UsersController {
-  static async get(_: Request, res: Response) {
-    return GoRestApi.get('users')
+  static async get(req: Request, res: Response) {
+    return api
+      .get(req.params.id)
+      .then((data) => res.json(data))
+      .catch((error) => res.status(error.status || 500).json(error))
+  }
+
+  static async create(req: Request, res: Response) {
+    return api
+      .post(req.body)
+      .then((data) => res.json(data))
+      .catch((error) => res.status(error.status || 500).json(error))
+  }
+
+  static async update(req: Request, res: Response) {
+    return api
+      .update(req.params.id, req.body)
+      .then((data) => res.json(data))
+      .catch((error) => res.status(error.status || 500).json(error))
+  }
+
+  static async remove(req: Request, res: Response) {
+    return api
+      .remove(req.params.id)
       .then((data) => res.json(data))
       .catch((error) => res.status(error.status || 500).json(error))
   }
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -4,5 +4,9 @@ import UsersController from '../controllers/UsersController'
 const router = Router()
 
 router.get('', UsersController.get)
+router.post('', UsersController.create)
+router.get('/:id', UsersController.get)
+router.put('/:id', UsersController.update)
+router.delete('/:id', UsersController.remove)
 
 export default router
